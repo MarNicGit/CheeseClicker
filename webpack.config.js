8132a982370b5412efadca1cc70c4cc1a7d8aded
MarNicGit <marnicgit@users.noreply.github.com>
2020-10-22 21:13:01 +0200

    renamed js output

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: ['./src/scripts/main.ts', './src/style/site.scss'],
     devtool: 'source-map',
     output: {
-      filename: 'clicky.js',
+      filename: 'cheeseclicker.js',
       path: path.resolve(__dirname, 'dist'),
     },
     module: {
